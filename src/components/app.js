d2a34c5a4ece49846715e356b1290c25ee43a2ba
Thomas Monfre <tmonfre@users.noreply.github.com>
2019-06-18 09:02:53 -0400

    fixed img height issue

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {
-  BrowserRouter as Router, Route,
+  BrowserRouter as Router, Route, Switch,
 } from 'react-router-dom';
-import { spring, AnimatedSwitch } from 'react-router-transition';
 
 import Home from './home';
 import Nav from './nav';
@@ -12,62 +11,20 @@ import Art from './art';
 import About from './about';
 import Fallback from './fallback';
 
-// we need to map the `scale` prop we define below
-// to the transform style property
-function mapStyles(styles) {
-  return {
-    opacity: styles.opacity,
-    transform: `scale(${styles.scale})`,
-  };
-}
-
-// wrap the `spring` helper to use a bouncy config
-function bounce(val) {
-  return spring(val, {
-    stiffness: 330,
-    damping: 22,
-  });
-}
-
-// child matches will...
-const bounceTransition = {
-  // start in a transparent, upscaled state
-  atEnter: {
-    opacity: 0,
-    scale: 1.2,
-  },
-  // leave in a transparent, downscaled state
-  atLeave: {
-    opacity: bounce(0),
-    scale: bounce(0.8),
-  },
-  // and rest at an opaque, normally-scaled state
-  atActive: {
-    opacity: bounce(1),
-    scale: bounce(1),
-  },
-};
-
 const App = () => {
   return (
     <Router>
       <div>
         <Nav />
         <div id="main-content">
-          <AnimatedSwitch
-            atEnter={bounceTransition.atEnter}
-            atLeave={bounceTransition.atLeave}
-            atActive={bounceTransition.atActive}
-            mapStyles={mapStyles}
-            className="route-wrapper"
-          >
+          <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/digital" component={Digital} />
             <Route path="/physical" component={Physical} />
             <Route path="/art" component={Art} />
             <Route path="/about" component={About} />
             <Route component={Fallback} />
-          </AnimatedSwitch>
+          </Switch>
         </div>
       </div>
     </Router>
